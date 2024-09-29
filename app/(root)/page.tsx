a3dbdd7f0e6a45b56d83f94d8e9d2d5d8346a46a
hm-huzaifa <mhuzaifa150@gmail.com>
2024-09-30 02:50:49 +0500

    Header Box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+    const loggedIn = {firstName: "Bro"}
     return (
-        <div>
-          Home
-        </div>
+        <section className="home">
+          <div className="home-content">
+              <header className="home-header">
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || "Not Bro"}
+                    subtext="Access and manage your account and transcations efficiently."
+                />
+              </header>
+              <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.69}
+
+              />
+          </div>
+        </section>
     );
 };
 
