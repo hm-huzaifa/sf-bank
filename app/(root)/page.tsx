92b2053646f277cfc2f30a0ed996fcc568b519ce
hm-huzaifa <mhuzaifa150@gmail.com>
2024-09-30 18:46:33 +0500

    sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-    const loggedIn = {firstName: "Bro"}
+    const loggedIn = {firstName: "Bro", lastName: "bro", email: "bro@bro.bro"};
     return (
         <section className="home">
-          <div className="home-content">
-              <header className="home-header">
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName || "Not Bro"}
-                    subtext="Access and manage your account and transcations efficiently."
-                />
-              </header>
-              <TotalBalanceBox
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250.69}
-              />
-          </div>
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || "Not Bro"}
+                        subtext="Access and manage your account and transcations efficiently."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.69}
+                    />
+                </header>
+                RECENT TRANSACTIONS
+            </div>
+            <RightSidebar user={loggedIn} transactions={[{}, {}]} banks={[{}, {}]}/>
         </section>
     );
 };
